184dd3e9bcc85ff20dcb1ef1366e6eab852677df
umit-gath <89274336+umit-gath@users.noreply.github.com>
2022-03-31 05:38:06 +0200

    test

---
+++ scripts/scripts.js
@@ -0,0 +1,46 @@
+
+// Preloader Function
+
+
+$(function(){ // this replaces document.ready
+    setTimeout(function(){
+      $('#preloader').fadeOut('slow', function() {
+        $(this).remove();
+        /// for preventing scrolling
+        $('body').css('overflow-y','auto');
+      });
+     }, 4000);
+  });
+
+
+
+    /// Navbar And Footer 
+    AOS.init({once: true,});
+
+    $(function(){
+      $("#nav-placeholder").load("navbar.html");
+    });
+
+    $(function(){
+      $("#footer-placeholder").load("footer.html");
+    });
+
+
+
+    /// Counters
+    var counter = 0;
+
+var x = setInterval(function() {
+  var distance = 1000;
+
+  counter = counter +1;
+  document.getElementById("counter-numb1").innerHTML = counter;
+  document.getElementById("counter-numb2").innerHTML = counter+20;
+  document.getElementById("counter-numb3").innerHTML = counter+90;
+
+
+  if (distance == counter) {
+    clearInterval(x);  }
+}, 50);
+
+
